7fb2b62a8bc5246418c743d97ef7848bb14b837d
Pavithra <pavithragamage916@gmail.com>
2022-09-05 23:51:44 +0530

    Update List Pages

--- app/Controllers/Pos.php
+++ app/Controllers/Pos.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Controllers;
+
 use CodeIgniter\CodeIgniter;
 
 class Pos extends BaseController
@@ -18,12 +19,72 @@ class Pos extends BaseController
     }
 
     // new invoice 
-    public function new_invoice(){
+    public function new_invoice()
+    {
         return view('new_invoice');
     }
 
     // all invoice 
-    public function all_invoices(){
+    public function all_invoices()
+    {
         return view('all_invoices');
+    }
+
+    // new quotes
+    public function new_quotes()
+    {
+        return view('new_quotes');
+    }
+
+    // all quotes
+    public function all_quotes()
+    {
+        return view('all_quotes');
+    }
+
+    // new category
+    public function new_category()
+    {
+        return view('new_category');
+    }
+
+    // all Categories
+    public function all_categories()
+    {
+        return view('all_categories');
+    }
+
+    // new items
+    public function new_items()
+    {
+        return view('new_items');
+    }
+
+    // all items
+    public function all_items()
+    {
+        return view('all_items');
+    }
+
+    // add client
+    public function add_client()
+    {
+        return view('add_client');
+    }
+
+     // all clients
+     public function all_clients()
+     {
+         return view('all_clients');
+     }
+
+    // add user
+    public function add_user(){
+        return view('add_user');
+    }
+
+    // all users
+    public function all_users(){
+        return view('all_users');
     }
 }
